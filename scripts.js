901faf51d69ee7d65d7e012ef7819b5f8eab87ac
eduardosb11 <barbosa.eduardo11@outlook.com>
2026-06-18 07:24:25 -0300

    listar clientes

--- scripts.js
+++ scripts.js
@@ -1,18 +1,25 @@
-const URL = "https://crudcrud.com/api/eed1546c602d49edbeac2430ef3160bf";
+const URL = "https://crudcrud.com/api/cf121622ec30459db6087ece944d1309/clientes";
 const form = document.querySelector("form");
+const atualizarListaBotao = document.querySelector("#lista-clientes > button");
+atualizarListaBotao.addEventListener("click", listarClientes);
 
-listarClientes();
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const nomeInput = document.querySelector("#nome");
+    const emailInput = document.querySelector("#email");
+    cadastrarCliente(nomeInput.value, emailInput.value);
+    nomeInput.value = "";
+    emailInput.value = "";
+});
 
 async function cadastrarCliente(nome, email) {
-    const cliente = { nome, email };
-
     try {
-        const resposta = await fetch(URL + "/clientes", {
+        const resposta = await fetch(URL, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify(cliente),
+            body: JSON.stringify({nome, email}),
         });
         if (!resposta.ok) {
             throw new Error(`Status ${resposta.status}`);
@@ -24,18 +31,23 @@ async function cadastrarCliente(nome, email) {
 
 async function listarClientes() {
     try {
-        const resposta = await fetch(URL + "/clientes", {
-            method: "GET",
-        });
+        const resposta = await fetch(URL);
         if (!resposta.ok) {
             throw new Error(`Status ${resposta.status}`);
         }
         const clientes = await resposta.json();
-        console.log(clientes);
+        clientes.forEach(listar);
     } catch (error) {
         console.error(error);
     }
+}
 
+function listar(cliente) {
+    const li = document.createElement("li");
+    li.innerText = `${cliente.nome} - ${cliente.email}`;
+
+    const ul = document.querySelector("#lista-clientes > ul");
+    ul.appendChild(li);
 }
 
 function excluirCliente() { }
